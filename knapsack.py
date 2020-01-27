48eddab7c0d30ba060a238a656c7884c253f7655
Afnan Sultan <sultan.afnan@gmail.com>
2020-01-27 00:47:16 -0500

    Add files via upload

--- knapsack.py
+++ knapsack.py
@@ -1,24 +1,28 @@
 from sys import stdin
 
 def main():
-    arr = [int(x) for x in stdin.readline().split()]
-    capacity = arr[0]
-    n = arr[1]
-    values = []
-    weights = []
-    for i in range(n):
-        value_weight = [int(x) for x in stdin.readline().split()]
-        values.append(value_weight[0])
-        weights.append(value_weight[1])
-    # Compute answer
-    indexes = knapsack(values, weights, n, capacity)
-    indexes.sort()
-    # Output answer
-    print(len(indexes))
-    for i, itemIndex in enumerate(indexes):
-        # Print indexOf index
-        print(itemIndex, end=" ")
-    print()
+    while True:
+        try:
+            arr = [int(x) for x in stdin.readline().split()]
+            capacity = arr[0]
+            n = arr[1]
+            values = []
+            weights = []
+            for i in range(n):
+                value_weight = [int(x) for x in stdin.readline().split()]
+                values.append(value_weight[0])
+                weights.append(value_weight[1])
+            # Compute answer
+            indexes = knapsack(values, weights, n, capacity)
+            indexes.sort()
+            # Output answer
+            print(len(indexes))
+            for i, itemIndex in enumerate(indexes):
+                # Print indexOf index
+                print(itemIndex, end=" ")
+            print()
+        except IndexError:
+            break
 
 def printTable(table):
     # print 2d list
@@ -29,7 +33,7 @@ def knapsack(values, weights, n, capacity):
     # first row and column are 0 so we do [n+1][n+1]
     table = [ [0] * (capacity+1) for _ in range(n+1) ]
     for i in range(1, n+1):
-        for w in range(0, n+1):
+        for w in range(0, capacity+1):
             # current weight plus previous weight
             totalWeight = w + weights[i-1]
             # total value plus previous value
